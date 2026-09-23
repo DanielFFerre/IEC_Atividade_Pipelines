e769b8f5f614d183fd67bc915359be496eda871b
Daniel Felipe Ferreira <daniel.ferreira53@fatec.sp.gov.br>
2026-09-23 13:59:08 +0000

    atulaiza loop calculadora

--- calculadora.py
+++ calculadora.py
@@ -15,7 +15,7 @@ def divisao(a, b):
         raise ValueError("Divisão por zero não é permitida")
     return a / b
 
-a = True
+p = True
 if __name__ == "__main__":  # pragma: no cover
     print("soma: + ")
     print("subtracao: - ")
@@ -23,18 +23,18 @@ if __name__ == "__main__":  # pragma: no cover
     print("divisao: / ")
     print("de um espaço entre os numeros e a operacao ")
     print("Exemplo: 10 + 10")
+    while(p == True):
+        a, operacao, b = input("digite a operacao que deseja fazer: ").split()
+        a = float(a)
+        b = float(b)
 
-    a, operacao, b = input("digite a operacao que deseja fazer: ").split()
-    a = float(a)
-    b = float(b)
-
-    if operacao == "+":
-        print(soma(a, b))
-    elif operacao == "-":
-        print(subtracao(a, b))
-    elif operacao == "*":
-        print(multiplicacao(a, b))
-    elif operacao == "/":
-        print(divisao(a, b))
-    else:
-        print("operacao invalida")
+        if operacao == "+":
+            print(soma(a, b))
+        elif operacao == "-":
+            print(subtracao(a, b))
+        elif operacao == "*":
+            print(multiplicacao(a, b))
+        elif operacao == "/":
+            print(divisao(a, b))
+        else:
+            print("operacao invalida")
